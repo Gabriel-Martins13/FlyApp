e74c5af32caadd82a8404bfea25355174ac29859
Aluno <Aluno@DESKTOP-O31M9M7>
2026-09-17 09:51:12 -0300

    Adicionar arquivos de projeto.

---
+++ AppFly/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppFly
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppFly/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppFly")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppFly.Pages")]
